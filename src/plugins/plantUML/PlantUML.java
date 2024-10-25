606bbfdf220e954036c60088988c88279fd559bd
nbourdi <nataliabourdh@gmail.com>
2024-10-25 17:55:59 +0300

    Initial commit

---
+++ src/plugins/plantUML/PlantUML.java
@@ -0,0 +1,20 @@
+package plugins.plantUML;
+
+import com.vp.plugin.VPPlugin;
+import com.vp.plugin.VPPluginInfo;
+
+public class PlantUML implements VPPlugin {
+
+	@Override
+	public void loaded(VPPluginInfo pluginInfo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unloaded() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
